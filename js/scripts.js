d2b45d50fa669e56705b0fc5aed7b12c91107cd2
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-02-02 01:32:30 +0100

    Correct js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var movies = [
     id: 1,
     title: 'Harry Potter',
     director: 'Reżyseria: 	Chris Columbus',
-    desc: 'film o czarodzieju',
+    desc: 'Opis: Film o czarodzieju',
     box: 'Boxoffice: 	$974 755 371',
     src: './pictures/harry.jpg'
   },
@@ -13,7 +13,7 @@ var movies = [
     id: 2,
     title: 'Król Lew',
     director: 'Reżyseria: 	Rob Minkoff, Roger Allers',
-    desc: 'Film o królu sawanny',
+    desc: 'Opis: Film o królu sawanny',
     box: 'Boxoffice: 	$974 755 371',
     src: './pictures/lion.jpg'
   },
@@ -21,7 +21,7 @@ var movies = [
     id: 3,
     title: 'Fernando',
     director: 'Reżyseria: 	Carlos Saldanha',
-    desc: 'Przyjacielski młody byczek przez pomyłkę zostaje zwerbowany do walk na arenie',
+    desc: 'Opis: Przyjacielski młody byczek przez pomyłkę zostaje zwerbowany do walk na arenie',
     box: 'Boxoffice: 	$128 942 249',
     src: './pictures/fernando.jpg'
   },
@@ -29,7 +29,7 @@ var movies = [
     id: 4,
     title: 'Shrek',
     director: 'Reżyseria: 	Andrew Adamson, Vicky Jenson',
-    desc: 'By odzyskać swój dom, brzydki ogr z gadatliwym osłem wyruszają uwolnić piękną księżniczkę.',
+    desc: 'Opis: By odzyskać swój dom, brzydki ogr z gadatliwym osłem wyruszają uwolnić piękną księżniczkę.',
     box: 'Boxoffice: 	$484 409 218',
     src: './pictures/shrek.jpg'
   },
@@ -37,7 +37,7 @@ var movies = [
     id: 5,
     title: 'Madagaskar',
     director: 'Reżyseria: 	Tom McGrath, Eric Darnell',
-    desc: 'Lew, zebra, hipopotam i żyrafa zostają wysłane statkiem z nowojorskiego zoo do Afryki. ',
+    desc: 'Opis: Lew, zebra, hipopotam i żyrafa zostają wysłane statkiem z nowojorskiego zoo do Afryki. ',
     box: 'Boxoffice: 	$532 680 671',
     src: './pictures/madagaskar.jpg'
   },
@@ -45,41 +45,19 @@ var movies = [
     id: 6,
     title: 'Kraina lodu',
     director: 'Reżyseria: 	Chris Buck, Jennifer Lee',
-    desc: 'Kiedy posiadająca moc kontroli nad śniegiem i lodem Elsa sprowadza srogą zimę na swoje królestwo, Anna wyrusza w podróż, aby odszukać siostrę oraz zakończyć pogodowe anomalie. ',
+    desc: 'Opis: Kiedy posiadająca moc kontroli nad śniegiem i lodem Elsa sprowadza srogą zimę na swoje królestwo, Anna wyrusza w podróż, aby odszukać siostrę oraz zakończyć pogodowe anomalie. ',
     box: 'Boxoffice: 		$1 274 219 009',
     src: './pictures/kraina.jpg'
   }
 ];
 
 
-var Movies = React.createClass({
+var MovieTitle = React.createClass({
   propTypes: {
-    items: React.PropTypes.array.isRequired,
+    title: React.PropTypes.string.isRequired
   },
   render: function() {
-    var movies = this.props.items.map(function(movie){
-      return React.createElement("li", {key: movie.id},
-          React.createElement(Movie, {item: movie}),
-          React.createElement(MovieDirector, {item: movie}),
-          React.createElement(MovieDescription, {item: movie}),
-          React.createElement(MovieBox, {item: movie}),
-          React.createElement(MoviePoster, {item: movie})
-      );
-    });
-    return (
-        React.createElement('div', {},
-            React.createElement('ul', {}, movies))
-    );
-  }
-});
-
-
-var Movie = React.createClass({
-  propTypes: {
-    movie: movies.title,
-  },
-  render: function() {
-    return React.createElement('h2', {className: "title"}, this.props.item.title);
+    return React.createElement('h2', {className: "title"}, this.props.title);
   }
 }
 );
@@ -87,10 +65,10 @@ var Movie = React.createClass({
 
 var MovieDirector = React.createClass({
   propTypes: {
-    item: movies.director,
+    director: React.PropTypes.string.isRequired
   },
   render: function() {
-    return React.createElement('p', {className: "director"}, this.props.item.director);
+    return React.createElement('p', {className: "director"}, this.props.director);
   }
 });
 
@@ -98,43 +76,66 @@ var MovieDirector = React.createClass({
 
 var MovieDescription = React.createClass({
   propTypes: {
-    item: movies.desc,
+    desc: React.PropTypes.string.isRequired
   },
   render: function() {
-    return React.createElement('p', {className: "description"}, this.props.item.desc);
+    return React.createElement('p', {className: "description"}, this.props.desc);
   }
 });
 
 
 var MovieBox = React.createClass({
   propTypes: {
-    item: movies.box,
+    box: React.PropTypes.string.isRequired
   },
   render: function() {
-    return React.createElement('p', {className: "box"}, this.props.item.box);
+    return React.createElement('p', {className: "box"}, this.props.box);
   }
 });
 
 
 var MoviePoster = React.createClass({
   propTypes: {
-    item: movies.src,
+    src: React.PropTypes.string.isRequired
   },
   render: function() {
-    return React.createElement('img', {src: this.props.item.src});
+    return React.createElement('img', {src: this.props.src});
   }
 });
+
+
+var Movie = React.createClass({
+  propTypes: {
+    movie: React.PropTypes.object.isRequired
+  },
+  render: function() {
+    return React.createElement("li", {key: this.props.movie.id},
+          React.createElement(MovieTitle, {title: this.props.movie.title}),
+          React.createElement(MovieDirector, {director: this.props.movie.director}),
+          React.createElement(MovieDescription, {desc: this.props.movie.desc}),
+          React.createElement(MovieBox, {box: this.props.movie.box}),
+          React.createElement(MoviePoster, {src: this.props.movie.src})
+      );
+    }
+});
+
 
 
 var MovieList = React.createClass({
+  propTypes: {
+    list: React.PropTypes.array.isRequired
+  },
   render: function(){
-    return React.createElement('div', {},
-      React.createElement('h1', {}, 'Lista filmów'),
-      React.createElement(Movies, {items: movies}, {})
-    );
-  }
+    var listElement = this.props.list.map(function(movie) {
+      return React.createElement(Movie, {movie: movie});
+		});
+      return React.createElement('div', {},
+        React.createElement('h1', {}, 'Lista filmów'),
+        React.createElement('ul', {}, listElement)
+      );
+    }
 });
 
-var movieList = React.createElement(MovieList);
+var movieList = React.createElement(MovieList, {list: movies});
 
 ReactDOM.render(movieList, document.getElementById('app'));
